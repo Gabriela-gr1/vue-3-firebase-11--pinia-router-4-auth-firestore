b935b21eb8e0859c5d01616641c08449368f94b7
Gabriela-gr1 <gabriela.gonzalez@gruporosa.com.mx>
2025-06-12 15:31:57 -0600

    firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgwN8rvjAmG9FGxAevy7773FzCdCLuqiQ",
-    authDomain: "vue-3-2025-28082.firebaseapp.com",
-    projectId: "vue-3-2025-28082",
-    storageBucket: "vue-3-2025-28082.firebasestorage.app",
-    messagingSenderId: "977883632107",
-    appId: "1:977883632107:web:3c26a7c3e7a859f0c189ae"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
